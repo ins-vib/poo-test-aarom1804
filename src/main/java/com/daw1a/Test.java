7d74a58d8a9aeb5a46e9f049c35204dce265d96e
Jeynner <jeynnerg@gmail.com>
2026-02-23 06:39:35 +0100

    Progress

---
+++ src/main/java/com/daw1a/Test.java
@@ -0,0 +1,20 @@
+package com.daw1a;
+
+import java.util.ArrayList;
+
+public class Test {
+    
+    ArrayList<Pregunta> pregunta = new ArrayList<>();
+    ArrayList<Integer> respostes = new ArrayList<>();
+
+    private int enter;
+
+    public Test(ArrayList<Pregunta> preguntes) {
+
+        preguntes.get(enter);
+
+    }
+
+    
+
+}
